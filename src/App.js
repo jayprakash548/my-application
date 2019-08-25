c1841a3903c3d8e771e480cf0f74158505c452d3
Jay Prakash Kumar <jay.kasyap@live.com>
2019-08-25 23:42:26 +0530

    Implemented state inside class and inside the constructor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 class App extends Component{
-  state = {
-    name: "Jay Prakash",
-    newName: this.props.name
-
-  }
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "Jay Prakash",
+      newName: this.props.name
+  
+    }
+  }  
   render(){
     return(
       <h1>Hello {this.state.name} and new name is {this.state.newName}</h1>
