a6a214d81c02192fcb29a1ec11446b40b9cc9cdf
Jay Prakash Kumar <jay.kasyap@live.com>
2019-08-26 23:05:02 +0530

    Event Handler using Class Components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+
 import React, { Component } from 'react'
 
 class App extends Component{
   constructor(props){
     super(props);
+
     this.state = {
-      name: "Jay Prakash",
-      newName: this.props.name
-  
+      name: 'Jay Singh',
+      Company: 'TechSci Research'
     }
-  }  
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    // handleClick = () =>{
+      //console.log("You have clicked", this);
+    //}
+    console.log("You have clicked", this);
+  }
   render(){
     return(
-      <h1>Hello {this.state.name} and new name is {this.state.newName}</h1>
+      <>
+        <h1>Hello Jay! you name{this.state.name} and Cpmpany Name {this.state.Company}</h1>
+        <button onClick={this.handleClick}>Click Me</button>
+      </>
     )
   }
 }
 
 export default App
- 
